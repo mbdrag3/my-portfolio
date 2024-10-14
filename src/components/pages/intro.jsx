c724f60787354cb46d23f1c243487a22f9a6a265
Michael Drag <mdrag@OBC-MDRAG.local>
2024-10-14 13:27:11 -0400

    added intro and removed video

--- src/components/pages/intro.jsx
+++ src/components/pages/intro.jsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react';
 import { useSpring, animated } from '@react-spring/web';
 import { useNavigate } from 'react-router-dom';
 import '../../styles/intro.css';
-import background from '../../assets/81676-576083179_small.mp4'; // Import the video
 
 function Intro() {
+
+  const videoURL = "https://www.dropbox.com/scl/fi/w8252z3xlfz3svr3b7lxr/81676-576083179_small.mp4?rlkey=qw98md3dk8bngcs52eo0i2ywd&st=amy14g7r&raw=1";
+
   const navigate = useNavigate();
   const [reveal, setReveal] = useState(false);
 
@@ -38,7 +40,7 @@ function Intro() {
   return (
     <div className="intro">
       <video className="background-video" autoPlay muted loop>
-        <source src={background} type="video/mp4" />
+        <source src={videoURL} type="video/mp4" />
       </video>
 
       {!reveal && (
